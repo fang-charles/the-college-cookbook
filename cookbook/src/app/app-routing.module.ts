e4a29d9111d70bf9270a576ad6df66381b5580a7
vivianpham8 <52727841+vivianpham8@users.noreply.github.com>
2020-03-01 22:09:40 -0500

    new pages

--- cookbook/src/app/app-routing.module.ts
+++ cookbook/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {RecipeComponent} from './recipe/recipe.component';
+import {HomeComponent} from './home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'submit', component: RecipeComponent },
+  { path: '**', redirectTo: '' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
