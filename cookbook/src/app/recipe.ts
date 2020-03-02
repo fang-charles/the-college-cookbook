ccd1672c90a6d8fa4b79ecc1f9da8270ec098187
vivianpham8 <52727841+vivianpham8@users.noreply.github.com>
2020-03-01 23:18:39 -0500

    create recipe form

---
+++ cookbook/src/app/recipe.ts
@@ -0,0 +1,8 @@
+export class Recipe {
+  name: string;
+  ingredients: Ingredients[];
+}
+
+export class Ingredients {
+  ingredient: string;
+}
--- cookbook/src/app/recipe/recipe.component.ts
+++ cookbook/src/app/recipe/recipe.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray, FormControl } from '@angular/forms';
+import { Recipe, Ingredients } from '../recipe'
 
 @Component({
   selector: 'app-recipe',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
+  recipe: FormGroup;
   ngOnInit(): void {
+    /* Initiate the form structure */
+    this.recipe = this.fb.group({
+      name: [],
+      ingredients: this.fb.array([this.fb.group({ingredient:''})])
+    })
   }
+
+  get Ingredients() {
+    return this.recipe.get('ingredients') as FormArray;
+  }
+
+  addIngredient() {
+    this.Ingredients.push(this.fb.group({ingredient:''}));
+  }
+
+  deleteIngredient(index) {
+    this.Ingredients.removeAt(index);
+  }
+
 
 }
