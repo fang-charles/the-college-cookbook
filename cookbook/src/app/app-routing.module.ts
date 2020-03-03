b96c71b63782fb3f6a90e6cfe44969afcd6d2d82
vivianpham8 <52727841+vivianpham8@users.noreply.github.com>
2020-03-02 20:10:00 -0500

    fix everything

--- cookbook/src/app/app-routing.module.ts
+++ cookbook/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {RecipeComponent} from './recipe/recipe.component';
-import {HomeComponent} from './home/home.component';
+import { RecipeComponent } from './recipe/recipe.component';
+import { HomeComponent } from './home/home.component';
 
 
 const routes: Routes = [
