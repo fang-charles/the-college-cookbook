e7ee61e0c7eb96abeadc536e9326d944c9878f0d
fang-charles <35390997+fang-charles@users.noreply.github.com>
2020-04-14 01:39:51 -0400

    edit

--- vhp7cj-csf8mb-assign3/editrecipes.php
+++ vhp7cj-csf8mb-assign3/editrecipes.php
@@ -22,72 +22,87 @@ include('header.html');
 
 
 	<div class="col-xs-12 col-sm-12 col-lg-12">
-<form action="<?php $_SERVER['PHP_SELF'] ?>" method="post" name="recipeForm">
-  <div class="form-group">
-  <div class="col-xs-6 col-xs-offset-3">
-    <label for="recipename">Recipe Name</label>
-    <input type="text" id="recipename" class="form-control" name="recipename" value="<?php $recipe['recipeName']?>"/>
-    <span class="error" id="recipename-note"></span>
-</div>
-  </div>
+		<form action="<?php $_SERVER['PHP_SELF'] ?>" method="post" name="recipeForm">
+			<div class="form-group">
+				<div class="col-xs-6 col-xs-offset-3">
+					<label for="recipename">Recipe Name</label>
+					<input type="text" id="recipename" class="form-control" name="recipename" value="<?php echo $recipe['recipeName'] ?>" />
+					<span class="error" id="recipename-note"></span>
+				</div>
+			</div>
 
-  <div class="form-group">
-    <label for="ingredient">Ingredients</label>
-    <input type="text" id="ingredient" class="form-control" name="ingredient[]" required value="<?php $recipe['ingredient1']?>"/>
-    <span class="error" id="ingredient-note"></span>
-  </div>
+			<div class="form-group">
+				<label for="ingredient">Ingredients</label>
+				<input type="text" id="ingredient" class="form-control" name="ingredient[]" required value="<?php echo $recipe['ingredient1'] ?>" />	
+				<span class="error" id="ingredient-note"></span>
+			</div>
+			<?php
+					for ($x = 2; $x <= $recipe['numIngredients']; $x++) {
+						$ingredientKey = "ingredient" . $x;
+						echo "<div class=\"form-group\">";
+						echo "<input type=\"text\" id=\"ingredient\" class=\"form-control\" name=\"ingredient[]\" required value=\"" . $recipe[$ingredientKey] . "\" />";
+						echo "</div>";
+					}
+				?>
 
-  <div id="newElementId"></div>
-  <input type="button" class="btn btn-light" id="add" value="Add Ingredient" onclick="addIngredient()"/>
 
-  <div class="form-group">
-    <label for="ingredient">Steps</label>
-    <input type="text" id="steps" class="form-control" name="steps[]" required value="<?php $recipe['step1']?>"/>
-    <span class="error" id="steps-note"></span>
-  </div>
 
-  <div id="newStep"></div>
-  <input type="button" class="btn btn-light" id="add1" value="Add Step" onclick="addStep()"/>
+			<div id="newElementId"></div>
+			<input type="button" class="btn btn-light" id="add" value="Add Ingredient" onclick="addIngredient()" />
 
-  <div>
-  <button class="btn btn-primary" formnovalidate>Update Recipe!</button>
-  </div>
-</form>
-</div>
+			<div class="form-group">
+				<label for="ingredient">Steps</label>
+				<input type="text" id="steps" class="form-control" name="steps[]" required value="<?php echo $recipe['step1'] ?>" />
+				<span class="error" id="steps-note"></span>
+			</div>
+			<?php
+					for ($x = 2; $x <= $recipe['numSteps']; $x++) {
+						$stepKey = "step" . $x;
+						echo "<div class=\"form-group\">";
+						echo "<input type=\"text\" id=\"steps\" class=\"form-control\" name=\"steps[]\" required value=\"" . $recipe[$stepKey] . "\" />";
+						echo "</div>";
+					}
+				?>
+
+			<div id="newStep"></div>
+			<input type="button" class="btn btn-light" id="add1" value="Add Step" onclick="addStep()" />
+
+			<div>
+				<button class="btn btn-primary" formnovalidate>Update Recipe!</button>
+			</div>
+		</form>
+	</div>
 </body>
 
 <script>
-  var numIngredients = 1;
-  var numSteps = 1;
-  var limit = 10;
+	var numIngredients = 1;
+	var numSteps = 1;
+	var limit = 10;
 
 
-  function addIngredient(){
-    if (numIngredients == limit)  {
-          alert("You can only add up to " + counter + " ingredients");
-     }
-     else {
-      var input = document.createElement("div");
-      input.setAttribute('class', 'form-group');
-      input.innerHTML = "<input type='text' id='ingredient' class='form-control' name='ingredient[]' required/>";
-      document.getElementById("newElementId").appendChild(input);
-      numIngredients++;
-      document.cookie = "numIngredients=" + numIngredients;
-     }
-  };
+	function addIngredient() {
+		if (numIngredients == limit) {
+			alert("You can only add up to " + counter + " ingredients");
+		} else {
+			var input = document.createElement("div");
+			input.setAttribute('class', 'form-group');
+			input.innerHTML = "<input type='text' id='ingredient' class='form-control' name='ingredient[]' required/>";
+			document.getElementById("newElementId").appendChild(input);
+			numIngredients++;
+			document.cookie = "numIngredients=" + numIngredients;
+		}
+	};
 
-  function addStep(){
-    if (numSteps == limit)  {
-          alert("You can only add up to " + limit + " steps");
-     }
-     else {
-      var input1 = document.createElement("div");
-      input1.setAttribute('class', 'form-group');
-      input1.innerHTML = "<input type='text' id='steps' class='form-control' name='steps[]' required/>";
-      document.getElementById("newStep").appendChild(input1);
-      numSteps++;
-      document.cookie = "numSteps=" + numSteps;
-     }
-  };
-
+	function addStep() {
+		if (numSteps == limit) {
+			alert("You can only add up to " + limit + " steps");
+		} else {
+			var input1 = document.createElement("div");
+			input1.setAttribute('class', 'form-group');
+			input1.innerHTML = "<input type='text' id='steps' class='form-control' name='steps[]' required/>";
+			document.getElementById("newStep").appendChild(input1);
+			numSteps++;
+			document.cookie = "numSteps=" + numSteps;
+		}
+	};
 </script>
